948f4e55aec38458f39baa98d84650be70e7c949
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2023-09-17 12:17:01 -0500

    feat:card inicial y filtro de personajes

---
+++ ApiMarvel/components/characterCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-card>
+        <v-img :src="character.thumbnail.path + '.' + character.thumbnail.extension"></v-img>
+        <v-card-title>{{ character.name }}</v-card-title>
+    </v-card>
+</template>
+
+<script setup>
+const props = defineProps({
+    character: {
+        type: Object,
+        required: true
+    }
+})
+</script>
--- ApiMarvel/pages/characters.vue
+++ ApiMarvel/pages/characters.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <div>
+    <v-row>
+      <v-col v-for="character in characters" :key="character.id" cols="12" sm="6" md="4">
+        <characterCard :character="character" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script setup>
@@ -11,10 +17,16 @@ const HASH = "426a36e6f780a1c9149fbd608ac5d007"
 const characters = ref([])
 
 const loadCharacters = async () => {
-    const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=${PUBLIC_API_KEY}&hash=${HASH}`;
-    const { data } = await axios.get(url);
-    characters.value = data
-    console.log(data)
+  const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=${PUBLIC_API_KEY}&hash=${HASH}`;
+  const { data } = await axios.get(url);
+  // Filtrar los personajes que tienen una imagen disponible
+  const charactersWithImages = data.data.results.filter(character => {
+    return character.thumbnail.path != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+  });
+
+  characters.value = charactersWithImages;
+  console.log(charactersWithImages);
 }
 
+loadCharacters();
 </script>
