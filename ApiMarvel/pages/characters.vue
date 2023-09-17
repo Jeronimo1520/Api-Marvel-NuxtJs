16f6deef506dafd394280284f59d19d11010b335
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2023-09-17 11:27:02 -0500

    feat:pagina inicio y coneccion con api marvel

---
+++ ApiMarvel/pages/characters.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script setup>
+import axios from "axios";
+
+const PUBLIC_API_KEY = 'cac443484e979ba71815a80c14030705';
+const HASH = "426a36e6f780a1c9149fbd608ac5d007"
+
+const characters = ref([])
+
+const loadCharacters = async () => {
+    const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=${PUBLIC_API_KEY}&hash=${HASH}`;
+    const { data } = await axios.get(url);
+    characters.value = data
+    console.log(data)
+}
+
+</script>
